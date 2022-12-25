abf4dcfe56ffa4a8e2913376f0245f25d5b72930
Nikhil Jha <nikhiljha@users.noreply.github.com>
2022-12-24 16:43:39 -0800

    :art: rewrite outline deployment

--- .transpire.py
+++ .transpire.py
@@ -1,187 +1,95 @@
-from transpire import helm
+from transpire import utils, helm
+from transpire.resources import Deployment, Ingress, Secret
 
-ingress = {
-    "enabled": True,
-    "ingressClassName": "cilium",
-    "annotations": {
-        "cert-manager.io/cluster-issuer": "letsencrypt",
-        "ingress.kubernetes.io/force-ssl-redirect": "true",
-        "kubernetes.io/tls-acme": "true",
-    },
-    "hosts": ["staffdocs.ocf.berkeley.edu"],
-    "tls": [{"hosts": ["staffdocs.ocf.berkeley.edu"], "secretName": "outline-tls"}],
-}
-
-ceph_store = {
-    "apiVersion": "ceph.rook.io/v1",
-    "kind": "CephObjectStore",
-    "metadata": {
-        "name": "outline",
-        "namespace": "outline",
-    }
-}
-
-ceph_user = {
-    "apiVersion": "ceph.rook.io/v1",
-    "kind": "CephObjectStoreUser",
-    "metadata": {
-        "name": "outline",
-        "namespace": "outline",
-    },
-    "spec": {
-        "store": "outline",
-        "displayName": "outline-user",
-        "quotas": {
-            "maxBuckets": "100",
-            "maxSize": "10G",
-            "maxObjects": "10000"
-        },
-        "capabilities": {
-            "user": "*",
-            "bucket": "*"
-        }
-    }
-}
-
-vault = {
-    "apiVersion": "ricoberger.de/v1alpha1",
-    "kind": "VaultSecret",
-    "metadata": {"name": "outline-secret"},
-    "spec": {
-        "keys": ["OIDC_CLIENT_SECRET", "SECRET_KEY", "UTILS_SECRET"],
-        "path": "kvv2/outline/outline",
-        "type": "Opaque",
-    },
-}
-
-postgres_vault = {
-    "apiVersion": "ricoberger.de/v1alpha1",
-    "kind": "VaultSecret",
-    "metadata": {"name": "postgres-secret"},
-    "spec": {
-        "keys": ["OIDC_CLIENT_SECRET", "SECRET_KEY", "UTILS_SECRET"],
-        "path": "kvv2/outline/postgres",
-        "type": "Opaque",
-    },
-}
-
-config = {
-    "kind": "ConfigMap",
-    "metadata": {"name": "outline", "namespace": "outline"},
-    "apiVersion": "v1",
-    "data": {
-        "AWS_REGION": "default",
-        "AWS_S3_ACL": "private",
-        "AWS_S3_FORCE_PATH_STYLE": "true",
-        "AWS_S3_UPLOAD_BUCKET_NAME": "outline",
-        "AWS_S3_UPLOAD_BUCKET_URL": "http://localhost:9000",
-        "AWS_S3_UPLOAD_MAX_SIZE": "26214400",
-        "DEFAULT_LANGUAGE": "en_US",
-        "ENABLE_UPDATES": "true",
-        "FORCE_HTTPS": "true",
-        "PGSSLMODE": "disable",
-        "PORT": "80",
-        "REDIS_URL": "redis://localhost:6379",
-        "SLACK_MESSAGE_ACTIONS": "true",
-        "URL": "https://staffdocs.ocf.berkeley.edu",
-        "OIDC_CLIENT_ID": "outline",
-        "OIDC_TOKEN_URI": "https://auth.ocf.berkeley.edu/auth/realms/ocf",
-        "OIDC_DISPLAY_NAME": "Sign in with SSO"
-    }
-}
-
-deploy = {
-    "apiVersion": "apps/v1",
-    "kind": "Deployment",
-    "metadata": {
-        "name": "outline",
-        "namespace": "outline"
-    },
-    "spec": {
-        "selector": {
-            "matchLabels": {"app": "outline"}
-        },
-        "strategy": {"type": "Recreate"},
-        "template": {
-            "metadata": {"labels": {"app": "outline"}},
-            "spec": {
-                "containers": [{
-                    "command": ["sh", "-c", "yarn ", "sequelize:migrate ", "--env ", "production-ssl-disabled ", "&& yarn start"],
-                    "envFrom": [
-                        {"configMapRef": {"name": "outline"}},
-                        {"secretRef": {"name": "outline-secret"}}
-                    ],
-                    "image": "outlinewiki/outline:latest",
-                    "name": "outline",
-                    "ports": [{"containerPort": 80}]}],
-
-                "volumes": [{"name": "data",
-                                "persistentVolumeClaim": {"claimName": "outline"}}]
-            }
-        }
-    }
-}                                       
-
-redis = {
-    "kind": "StatefulSet",
-    "metadata": {"name": "outline-redis", "namespace": "outline"},
-    "spec": {
-        "selector": {"matchlabels": {"app": "outline"}},
-        "serviceName": "postgres",
-        "template": {
-            "metadata": {"labels": {"app": "outline"}},
-            "spec": {
-                "containers": {
-                    "image": "redis:latest",
-                    "name": "redis",
-                    "ports": [{"containerPort": 6379}]
-                }
-            }
-        }
-    }
-}
-
-postgres_config = {
-    "kind": "ConfigMap",
-    "metadata": {"name": "outline-postgres", "namespace": "outline"},
-    "apiVersion": "v1",
-    "data": {
-        "POSTGRES_DB": "outline",
-        "POSTGRES_USER": "outline"
-    }
-}
-
-
-postgres = {
-    "kind": "StatefulSet",
-    "metadata": {"name": "outline-postgres", "namespace": "outline"},
-    "spec": {
-        "selector": {"matchlabels": {"app": "outline"}},
-        "serviceName": "postgres",
-        "template": {
-            "metadata": {"labels": {"app": "outline"}},
-            "spec": {
-                "containers": {
-                    "name": "postgres",
-                    "image": "postgres:latest",
-                    "ports": [{"containerPort": 5432}],
-                    "volumeMounts": [{
-                        "mountPath": "/var/lib/postgresql/data",
-                        "name": "data",
-                        "subPath": "postgres"}],
-                    "envFrom": [{"configMapRef": {"name": "outline-postgres"}, "secretRef": "postgres-secret"}]
-                }
-            }
-        }
-    }
-}
+name = "outline"
+namespace = name
+# A lil' bit of magic to get the adjacent versions.toml file. DWAI.
+versions = utils.get_versions(__file__)
+image = f"docker.io/outlinewiki/outline:{versions[name]['version']}"
 
 def objects():
-    yield ingress
-    yield ceph_store
-    yield ceph_user
-    yield vault
-    yield postgres_vault
-    yield redis
-    yield postgres
-    yield deploy
+    # Create an Ingress (a piece of standard configuration for web proxies).
+    # This will configure the Envoy listening at 169.229.226.81 to forward
+    # docs.ocf.berkeley.edu to the Service called "outline-web" on port 80.
+    yield Ingress.simple(
+        host="docs.ocf.berkeley.edu",
+        service_name=f"{name}-web",
+        service_port=80,
+    )
+
+    # This returns a Kubernetes secret object, which actually contains secret
+    # data! Not to worry though, in production, transpire will intercept these
+    # and deploy a VaultSecret. You can use this to generate default values
+    # randomly, if possible. That makes your transpire module usable by others,
+    # and slightly speeds up bootstrapping.
+    yield Secret.simple(
+        name=name,
+        # If you can automatically generate these, do so here.
+        # Then you can just `transpire secret push` these to Vault!
+        string_data={
+            "OIDC_CLIENT_SECRET": "",
+            "SECRET_KEY": "",
+            "UTILS_SECRET": "",
+            "AWS_ACCESS_KEY_ID": "",
+            "AWS_SECRET_ACCESS_KEY": "",
+            # You have to specify the user/pass in the URL so this has to go in a secret.
+            "DATABASE_URL": "",
+            "REDIS_URL": "",
+        },
+    )
+
+    # Configuration details for outline-- notice how these are injected
+    # as environment variables into the Deployment!
+    yield {
+        "apiVersion": "v1",
+        "kind": "ConfigMap",
+        "metadata": {"name": name},
+        "data": {
+            "AWS_REGION": "default",
+            "AWS_S3_ACL": "private",
+            "AWS_S3_FORCE_PATH_STYLE": "true",
+            "AWS_S3_UPLOAD_BUCKET_NAME": "outline",
+            # TODO: Configure a global object storage gateway.
+            "AWS_S3_UPLOAD_BUCKET_URL": "http://localhost:9000",
+            "AWS_S3_UPLOAD_MAX_SIZE": "26214400",
+            "DEFAULT_LANGUAGE": "en_US",
+            "ENABLE_UPDATES": "true",
+            "FORCE_HTTPS": "true",
+            "PGSSLMODE": "disable",
+            "PORT": "80",
+            "SLACK_MESSAGE_ACTIONS": "true",
+            "URL": "https://docs.ocf.berkeley.edu",
+            "OIDC_CLIENT_ID": "outline",
+            "OIDC_TOKEN_URI": "https://auth.ocf.berkeley.edu/auth/realms/ocf",
+            "OIDC_DISPLAY_NAME": "Sign in with OCF"
+        }
+    }
+
+    # This will create a container, and watch it if it dies to continually
+    # restart it. Here we use a custom command, but we should probably build off
+    # the Dockerfile and override it instead of doing this.
+    yield Deployment.simple(
+        name=name,
+        image=image,
+        command=["sh", "-c", "yarn ", "sequelize:migrate ", "--env ", "production-ssl-disabled ", "&& yarn start"],
+        ports=[80],
+        configs_env=[name],
+        secrets_env=[name],
+    )
+
+    # This deploys everything you need to run postgres! That was easy.
+    # TODO: Put configuration in the values={} dict.
+    yield helm.build_chart_from_versions(
+        name="postgresql",
+        versions=versions,
+        # <https://github.com/bitnami/charts/tree/main/bitnami/postgresql>
+        values={},
+    )
+
+    yield helm.build_chart_from_versions(
+        name="redis",
+        versions=versions,
+        # <https://github.com/bitnami/charts/tree/main/bitnami/redis>
+        values={},
+    )
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # outline
-configuration for ocf outlinewiki deployment
+
+OCF Deployment of Outline -> A fast, collaborative, knowledge base built using React and Node.js.
+
+Use: [docs.ocf.berkeley.edu](https://docs.ocf.berkeley.edu/) |
+Image: [docker.io/outlinewiki/outline](https://hub.docker.com/r/outlinewiki/outline) |
+Upstream: [github.com/outline/outline](https://github.com/outline/outline)
+
